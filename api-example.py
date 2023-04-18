4b06ef6836882c8cfc7a3f7190a9d0562c59c959
elumuu <107090224+elumuu@users.noreply.github.com>
2023-04-18 10:34:02 +0200

    Create api-example.py

---
+++ api-example.py
@@ -0,0 +1,46 @@
+'''
+This is an example on how to use the API for oobabooga/text-generation-webui.
+Make sure to start the web UI with the following flags:
+python server.py --model MODEL --listen --no-stream
+Optionally, you can also add the --share flag to generate a public gradio URL,
+allowing you to use the API remotely.
+'''
+import json
+
+import requests
+
+# Server address
+server = "127.0.0.1"
+
+# Generation parameters
+# Reference: https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig
+params = {
+    'max_new_tokens': 200,
+    'do_sample': True,
+    'temperature': 0.72,
+    'top_p': 0.73,
+    'typical_p': 1,
+    'repetition_penalty': 1.1,
+    'encoder_repetition_penalty': 1.0,
+    'top_k': 0,
+    'min_length': 0,
+    'no_repeat_ngram_size': 0,
+    'num_beams': 1,
+    'penalty_alpha': 0,
+    'length_penalty': 1,
+    'early_stopping': False,
+    'seed': -1,
+}
+
+# Input prompt
+prompt = "What I would like to say is the following: "
+
+payload = json.dumps([prompt, params])
+
+response = requests.post(f"http://{server}:7860/run/textgen", json={
+    "data": [
+        payload
+    ]
+}).json()
+
+reply = response["data"][0]
