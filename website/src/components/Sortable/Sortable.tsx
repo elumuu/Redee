b412e2f23d479ae685bb0d8a1bcbc8beedede6c7
elumuu <107090224+elumuu@users.noreply.github.com>
2024-06-18 11:59:45 +0800

    Create Sortable.tsx

---
+++ website/src/components/Sortable/Sortable.tsx
@@ -0,0 +1,61 @@
+import { DndContext, PointerSensor, TouchSensor, closestCenter, useSensor, useSensors } from "@dnd-kit/core";
+import { ReactNode, useEffect, useState } from "react";
+import { SortableContext, arrayMove, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import type { DragEndEvent } from "@dnd-kit/core/dist/types/events";
+import { Flex } from "@chakra-ui/react";
+import { SortableItem } from "./SortableItem";
+
+export interface SortableProps {
+  items: ReactNode[];
+  onChange: (newSortedIndices: number[]) => void;
+}
+
+interface SortableItems {
+  id: number;
+  originalIndex: number;
+  item: ReactNode;
+}
+
+export const Sortable = ({ items, onChange }: SortableProps) => {
+  const [itemsWithIds, setItemsWithIds] = useState<SortableItems[]>([]);
+
+  useEffect(() => {
+    setItemsWithIds(
+      items.map((item, idx) => ({
+        item,
+        id: idx + 1, // +1 because dndtoolkit has problem with "falsy" ids
+        originalIndex: idx,
+      }))
+    );
+  }, [items]);
+
+  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
+  return (
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={itemsWithIds} strategy={verticalListSortingStrategy}>
+        <Flex direction="column" gap={2}>
+          {itemsWithIds.map(({ id, item }) => (
+            <SortableItem key={id} id={id}>
+              {item}
+            </SortableItem>
+          ))}
+        </Flex>
+      </SortableContext>
+    </DndContext>
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setItemsWithIds((items) => {
+      const oldIndex = items.findIndex((x) => x.id === active.id);
+      const newIndex = items.findIndex((x) => x.id === over.id);
+      const newArray = arrayMove(items, oldIndex, newIndex);
+      onChange(newArray.map((item) => item.originalIndex));
+      return newArray;
+    });
+  }
+};
