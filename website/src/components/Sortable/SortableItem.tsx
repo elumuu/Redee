ed193bbb37c41224faeb34173dd9fe954493ae12
elumuu <107090224+elumuu@users.noreply.github.com>
2024-10-03 18:17:23 +0800

    Create SortableItem.tsx

---
+++ website/src/components/Sortable/SortableItem.tsx
@@ -0,0 +1,25 @@
+import { CSS } from "@dnd-kit/utilities";
+import { PropsWithChildren } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+
+export const SortableItem = ({ children, id }: PropsWithChildren<{ id: number }>) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    touchAction: "none",
+  };
+
+  return (
+    <li
+      className="rounded-lg shadow-md p-4 bg-white hover:bg-slate-50"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      {children}
+    </li>
+  );
+};
